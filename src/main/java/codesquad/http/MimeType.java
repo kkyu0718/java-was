4f6d39908c5c00d5368407c3f772d9b513be4824
kimkw <kyuwon0718@gmail.com>
2024-07-03 21:55:01 +0900

    feat: mime type 정의

---
+++ src/main/java/codesquad/http/MimeType.java
@@ -0,0 +1,40 @@
+package codesquad.http;
+
+import java.nio.file.Path;
+
+public enum MimeType {
+    HTML("html", "text/html"),
+    CSS("css", "text/css"),
+    JS("js", "text/javascript"),
+    ICO("ico", "image/x-icon"),
+    PNG("png", "image/png"),
+    JPG("jpg", "image/jpeg"),
+    SVG("svg", "image/svg+xml");
+
+    private String ext;
+    private String mimeType;
+
+    MimeType(String ext, String mimeType) {
+        this.ext = ext;
+        this.mimeType = mimeType;
+    }
+
+    public String getExt() {
+        return ext;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public static MimeType fromExt(Path fileName) {
+        String ext = String.valueOf(fileName).split("\\.", 2)[1];
+        for (MimeType mimeType : MimeType.values()) {
+            if (ext.equals(mimeType.ext)) {
+                return mimeType;
+            }
+        }
+
+        throw new IllegalArgumentException("No mimetype enum constant with extension" + ext);
+    }
+}
