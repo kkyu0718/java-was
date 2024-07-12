e28608cf6c837083468f7f178d98f2e65499a272
KyuWon  Kim <kyuwon0718@gmail.com>
2024-07-12 14:24:29 +0900

    hotfix: path 에 / 가 한번더 들어가는 오류 수정

--- src/test/java/codesquad/reader/StaticFileReaderTest.java
+++ src/test/java/codesquad/reader/StaticFileReaderTest.java
@@ -16,7 +16,7 @@ class StaticFileReaderTest {
 
     @Test
     void readFileLines_메서드가_파일내용을_정확히_String으로_반환하는지_검증한다() throws IOException {
-        String content = reader.readFileLines("index.html");
+        String content = reader.readFileLines("/index.html");
 
         Assertions.assertNotNull(content);
         Assertions.assertTrue(content.contains("<!DOCTYPE html>"));
@@ -27,14 +27,14 @@ class StaticFileReaderTest {
     @Test
     void readFileLines_메서드가_존재하지_않는_파일에_대해_예외를_던지는지_검증한다() {
         Assertions.assertThrows(IOException.class, () -> {
-            reader.readFileLines("non_existent_file.html");
+            reader.readFileLines("/non_existent_file.html");
         });
     }
 
     @Test
     void readFileLines_메서드가_빈_파일을_처리할_수_있는지_검증한다() throws IOException {
         // 빈 파일이 'empty.txt'라는 이름으로 클래스패스에 존재한다고 가정
-        String content = reader.readFileLines("empty.txt");
+        String content = reader.readFileLines("/empty.txt");
 
         Assertions.assertEquals("", content);
     }
