5dc372c9fbab864745f7870c6602b2ef90b1a231
KyuWon  Kim <kyuwon0718@gmail.com>
2024-07-16 13:58:19 +0900

    feat: 데이터베이스 초기화 schema.sql 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS Comment;
+DROP TABLE IF EXISTS Post;
+DROP TABLE IF EXISTS User;
+
+CREATE TABLE User (
+    user_id VARCHAR(20) PRIMARY KEY ,
+    password VARCHAR(50),
+    name VARCHAR(50),
+    email VARCHAR(100)
+                   );
+
+
+CREATE TABLE Post (
+                      id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                      user_id VARCHAR(20) NOT NULL,
+                      content TEXT NOT NULL,
+                      FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
+CREATE TABLE Comment (
+                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                         post_id BIGINT NOT NULL,
+                         user_id VARCHAR(20) NOT NULL,
+                         content TEXT NOT NULL,
+                         FOREIGN KEY (post_id) REFERENCES Post(id),
+                         FOREIGN KEY (user_id) REFERENCES User(user_id)
+);
+
