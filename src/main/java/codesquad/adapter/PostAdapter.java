cedefd816aee9c140a09f7bcbcc018a77cb7d39a
kimkw <kyuwon0718@gmail.com>
2024-07-16 23:24:47 +0900

    feat: PostAdapter 코드 추가

---
+++ src/main/java/codesquad/adapter/PostAdapter.java
@@ -0,0 +1,37 @@
+package codesquad.adapter;
+
+import codesquad.annotation.RequestMapping;
+import codesquad.http.*;
+import codesquad.model.dao.PostCreateDao;
+import codesquad.service.PostServiceSpec;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PostAdapter implements Adapter {
+    private static final Logger logger = LoggerFactory.getLogger(PostAdapter.class);
+
+    private PostServiceSpec postService;
+
+    public PostAdapter(PostServiceSpec postService) {
+        this.postService = postService;
+    }
+
+    @Override
+    public boolean supports(String path) {
+        return path.startsWith("/posts");
+    }
+
+    @RequestMapping(path = "/posts/create", method = HttpMethod.POST)
+    public HttpResponse createPost(HttpRequest request) {
+        logger.debug("createPost start");
+        HttpBody body = request.getBody();
+        PostCreateDao dao = body.parse(PostCreateDao.class);
+
+        postService.createPost(dao);
+        logger.debug("Post created successfully");
+
+        return new HttpResponse.Builder(request, HttpStatus.OK)
+                .build();
+    }
+
+}
