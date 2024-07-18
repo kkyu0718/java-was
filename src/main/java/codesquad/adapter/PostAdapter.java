6f017760eb3c361f35f55698ced3fbb2a7631a2f
kimkw <kyuwon0718@gmail.com>
2024-07-19 00:12:32 +0900

    feat: UploadUtils 분리
    시스템 홈 경로에 이미지 저장하는 utils 분리

--- src/main/java/codesquad/adapter/PostAdapter.java
+++ src/main/java/codesquad/adapter/PostAdapter.java
@@ -2,36 +2,19 @@ package codesquad.adapter;
 
 import codesquad.annotation.RequestMapping;
 import codesquad.http.*;
-import codesquad.model.MultipartFile;
 import codesquad.model.dao.PostCreateDao;
 import codesquad.service.PostServiceSpec;
+import codesquad.utils.UploadUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.UUID;
-
 public class PostAdapter implements Adapter {
     private static final Logger logger = LoggerFactory.getLogger(PostAdapter.class);
-    private static final String STATIC_DIR = "/static";
-    private static final String UPLOAD_DIR = System.getProperty("user.home") + File.separator + "uploads";
     private PostServiceSpec postService;
 
     public PostAdapter(PostServiceSpec postService) {
         this.postService = postService;
-        createUploadDir();
-    }
-
-    private void createUploadDir() {
-        File uploadDir = new File(UPLOAD_DIR);
-        if (!uploadDir.exists()) {
-            if (!uploadDir.mkdirs()) {
-                logger.error("Failed to create upload directory");
-            }
-        }
+        UploadUtils.createUploadDir();
     }
 
     @Override
@@ -48,7 +31,7 @@ public class PostAdapter implements Adapter {
         logger.debug("PostCreateDao : {}", dao);
 
         if (dao.getFile() != null) {
-            String savedFilePath = saveFileToLocalResource(dao.getFile());
+            String savedFilePath = UploadUtils.saveFileToLocalResource(dao.getFile());
             dao.setImageUrl(savedFilePath);  // 저장된 파일 경로를 DAO에 설정
             logger.debug("savedFilePath : {}", savedFilePath);
         }
@@ -61,16 +44,5 @@ public class PostAdapter implements Adapter {
                 .build();
     }
 
-    private String saveFileToLocalResource(MultipartFile file) {
-        String uniqueFileName = UUID.randomUUID() + "_" + file.getFileName();
-        File targetFile = new File(UPLOAD_DIR, uniqueFileName);
-        try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(targetFile))) {
-            bos.write(file.getContent(), 0, file.getContent().length);
-        } catch (IOException e) {
-            logger.error("Error saving file", e);
-        }
 
-        logger.debug("File saved at: {}", targetFile.getAbsolutePath());
-        return "/uploads/" + uniqueFileName;
-    }
 }
